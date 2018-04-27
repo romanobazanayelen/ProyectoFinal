a725e29b848597920fc254b19239974e3df4eb9a
Ayelen <Ayelen @192.168.0.81>
2018-04-26 21:21:08 -0300

    Conector 2

---
+++ Proyecto-Final/src/capadatos/Conector.java
@@ -0,0 +1,17 @@
+package capadatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conector {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		Connection con = null;
+		String sURL = "jdbc:mysql://localhost:3306/proyectofinal?autoReconnect=true&useSSL=false";
+		con = DriverManager.getConnection(sURL,"root","3991");
+	System.out.println(con.toString());
+	}
+}
+
