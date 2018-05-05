eef04689b6a84a7902e7e8bbbfac9a844a7c05f5
nata_ <nata_@LAPTOP-8K4T7IEH>
2018-05-05 19:16:22 -0300

    cambio conexion base de datos

--- Proyecto-Final/src/capadatos/Conector.java
+++ Proyecto-Final/src/capadatos/Conector.java
@@ -5,13 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conector {
-
+	
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
-		Connection con = null;
-		String sURL = "jdbc:mysql://localhost:3306/proyectofinal?autoReconnect=true&useSSL=false";
+		Connection con = null;			
+			
+		String sURL = "jdbc:mysql://localhost:3306/proyectofinal?autoReconnect=true&useSSL=false&serverTimezone=UTC";
+		
 		con = DriverManager.getConnection(sURL,"root","3991");
-	System.out.println(con.toString());
+		
+		System.out.println(con.toString());
+		
+	
 	}
 }
 
